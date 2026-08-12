0fa8ad368d6786f500c348a02d18481031396b87
Thash <afthashsahal123@gmail.com>
2026-08-12 15:13:43 +0930

    added splash preserve

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,15 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'router/router.dart';
 import 'theme/app_theme.dart';
+import '../providers/auth/auth_notifier.dart';
 
-class CloutgridApp extends ConsumerWidget {
+class CloutgridApp extends ConsumerStatefulWidget {
   const CloutgridApp({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<CloutgridApp> createState() => _CloutgridAppState();
+}
+
+class _CloutgridAppState extends ConsumerState<CloutgridApp> {
+  @override
+  void initState() {
+    super.initState();
+    ref.listenManual(authProvider, (previous, next) {
+      if (!next.isLoading) {
+        FlutterNativeSplash.remove();
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final router = ref.watch(appRouterProvider);
 
     return AnnotatedRegion<SystemUiOverlayStyle>(
--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 import 'app/app.dart';
 
-void main() {
-  WidgetsFlutterBinding.ensureInitialized();
+void main() async {
+  final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
 
-  SystemChrome.setEnabledSystemUIMode(
-    SystemUiMode.edgeToEdge
-  );
+  await SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
 
-  runApp(
-    const ProviderScope(
-      child: CloutgridApp(),
-    ),
-  );
+  runApp(const ProviderScope(child: CloutgridApp()));
 }
