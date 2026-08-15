3e3db85b929fc86a46a532da9a06ad01332c8ca5
Thash <afthashsahal123@gmail.com>
2026-08-15 21:43:55 +0930

    first ios build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,6 @@
+import java.util.Properties
+import java.io.FileInputStream
+
 plugins {
     id("com.android.application")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
